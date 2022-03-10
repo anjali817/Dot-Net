d4beed3247cb4164d5fcbd99043a8570a88dd631
anjali817 <99798215+anjali817@users.noreply.github.com>
2022-03-10 18:42:40 +0530

    .net first project viewbag & viewdata

---
+++ NetIndex.cs
@@ -0,0 +1,25 @@
+﻿<h1>===========ViewBag=========</h1>
+<h1>@ViewBag.abc</h1>
+<h1>@ViewBag.test</h1>
+<h1>@ViewBag.test1</h1>
+<h1>@ViewBag.test2</h1>
+
+<ul>
+    @foreach (var item in ViewBag.list)
+    {
+        <li>@item</li>
+    }
+</ul>
+
+<h1>=========ViewData=========</h1>
+<h1>@ViewData["abc"]</h1>
+<h1>@ViewData["test"]</h1>
+<h1>@ViewData["test1"]</h1>
+<h1>@ViewData["test2"]</h1>
+<h1>@ViewData["test3"]</h1>
+<ul>
+    @foreach (var item in (List<string>)(ViewData["ListData"]))
+    {
+        <li>@item</li>
+    }
+</ul>
---
+++ NetViewbag-data.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codermvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.abc = "Hii coder";
+            ViewBag.test = 1234;
+            ViewBag.test1 = 23.5f;
+            ViewBag.test2 = DateTime.Now;
+            ViewBag.list = new List<string>()
+            {
+                "India",
+                "Pak",
+                "USA"
+            };
+
+            ViewData["abc"] = "View Data value";
+            ViewData["test"] = 8888;
+            ViewData["test1"] = 11.7f;
+            ViewData["test2"] = 34.98;
+            ViewData["test3"] = DateTime.Now;
+            ViewData["ListData"] = new List<String>()
+            {
+                "India",
+                "Pak",
+                "USA"
+            };
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+        }
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
